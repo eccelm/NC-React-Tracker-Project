b9b159bc0f7084d96bce8b90eb02d9ee1f1d0c59
Martha <mleccles@outlook.com>
2021-02-05 22:47:13 +0000

    adds header and footer

---
+++ src/Components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="nav-div">
+    <div style={{height:`100px`, backgroundColor: `pink`, bottom: `0`, position:`fixed`, width:`100vw`}}>I AM THE FOOTER</div>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ src/Components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="nav-div">
+    <div style={{height:`100px`, backgroundColor: `goldenrod`}}>I AM THE HEADER</div>
+    </div>
+  );
+};
+
+export default Header;
