85f3035609e8300fb7313ac03787c4078ce71018
Martha <mleccles@outlook.com>
2021-02-05 16:23:47 +0000

    removes div from App and moves revelant css to body

--- src/App.js
+++ src/App.js
@@ -7,15 +7,14 @@ import Blocks from "./Components/Blocks";
 import Students from "./Components/Students";
 
 function App() {
+  
   return (
-    // is div needed now?
-    <div className="App">
       <Router>
         <HomePage path="/" />
         <Blocks path="/blocks" />
         <Students path="/students" />
       </Router>
-    </div>
+  
   );
 }
 
