0771d4155cbed6650b4a9b1c495f9bbb25aef811
Martha <mleccles@outlook.com>
2021-02-10 15:07:05 +0000

    testings adding .eslintcache

---
+++ src/Components/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Link } from "@reach/router";
+/*
+Ignore navbar for now figure out when doing other buttons 
+
+*/
+const Header = () => {
+  return (
+    <div className="nav-div" style={{height: `100px`, backgroundColor: `lavender`}}>
+      <h1 >I am a test</h1>
+    </div>
+  );
+};
+
+export default Header;
--- src/Components/HomePage.js
+++ src/Components/HomePage.js
@@ -1,9 +1,10 @@
 import React from "react";
 import { Link } from "@reach/router";
-
+import Header from "./Header";
 const HomePage = () => {
   return (
     <div className="homepage-wrapper">
+      <Header />
       <h1>Welcome to Northcoders Student Tracker</h1>
       <Link to="/students">
         <button className="homepage-button">Students</button>
