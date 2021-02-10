31e58bdb77443632bfcee362632f66907dff2045
Martha <mleccles@outlook.com>
2021-02-10 18:26:08 +0000

    adds async axios GET for a single student to api.js

--- src/Components/StudentCard.js
+++ src/Components/StudentCard.js
@@ -1,14 +1,13 @@
 import React from "react";
 
 const StudentCard = (props) => {
-  //console.log("inside the Student card");
-  //console.log(props);
+  console.log(props);
   const { _id, name, startingCohort, currentBlock } = props;
 
   return (
     <li className="student-card">
       <h2>
-        {name} ID: {_id}
+        {name}
       </h2>
       <p>Starting Cohort: {startingCohort}</p>
       <p>Current Block: {currentBlock}</p>
--- src/api.js
+++ src/api.js
@@ -4,14 +4,10 @@ const trackerApi = axios.create({
   baseURL: "https://nc-student-tracker.herokuapp.com/api",
 });
 
-
 export async function fetchStudents(){
    const response = await trackerApi
    .get("/students")
-   //console.log(students, Object.keys(students))
    const {students} = response.data;
-   console.log(students)
-   console.log("I'm second")
    return students;
 }
 
@@ -22,22 +18,8 @@ export async function fetchBlocks(){
   return blocks;
 }
 
-// fetchStudentsList = () => {
-//    return axios
-//      .get("https://nc-student-tracker.herokuapp.com/api/students")
-//      .then((response) => {
-//        console.log(response);
-//        return response.data.students;
-//      });
-//  };
-
-
-//  fetchBlocks = () => {
-//    return axios
-//      .get("https://nc-student-tracker.herokuapp.com/api/blocks")
-//      .then((response) => {
-//        console.log(response);
-//        return response.data.blocks;
-//      });
-//  };
-
+export const getStudentById = async (id) => {
+  const response = await trackerApi.get(`/${id}`)
+  const {student} = response.data;
+  return student;
+};
