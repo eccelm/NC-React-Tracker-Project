da705cc88c278d3303b270b8c6cc844caeafcd6e
Martha <mleccles@outlook.com>
2021-02-12 12:09:29 +0000

    adds advanceStudent block advancing functionality

--- src/Components/StudentCard.js
+++ src/Components/StudentCard.js
@@ -3,7 +3,7 @@ import React from "react";
 const StudentCard = (props) => {
   //console.log(props, "func should be here");
   // need id from props for delete and patch props.removeStudent, props.graduateStudent
-  const { _id, name, startingCohort, currentBlock, removeStudent } = props;
+  const { _id, name, startingCohort, currentBlock, removeStudent, advanceStudent } = props;
 
   return (
     <li className="student-card" style={{ border: `2px solid`, listStyleType: `none`, display:`flex`}}>
@@ -14,7 +14,9 @@ const StudentCard = (props) => {
       <p>{_id}</p>
       <p>Starting Cohort: {startingCohort}</p>
       <p>Current Block: {currentBlock}</p>
-      <button id="patch-student-btn">Next Block</button>
+      <button id="patch-student-btn" onClick={()=> {
+        advanceStudent(_id, true)
+      }}>Next Block</button>
     </li>
   );
 };
