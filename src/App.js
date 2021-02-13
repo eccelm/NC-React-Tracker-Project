293dbd48583bd6218958b1d4f65c2b27a2a18609
Martha <mleccles@outlook.com>
2021-02-13 19:28:35 +0000

    creates sass file - must stay below version5

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import "./App.css";
-
+import "./Appsass.scss"
 import { Router } from "@reach/router";
-//import Nav from "./Components/NavBar";
 import HomePage from "./Components/HomePage";
 import Students from "./Components/Students";
 
 function App() {
   return (
-    // is div needed now?
+ 
     <div className="App">
       <Router>
         <HomePage path="/" />
